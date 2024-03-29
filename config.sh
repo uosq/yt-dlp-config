501fcd43acc1b35d91d78f0ab561be89f6b688aa
navet <159667110+uosq@users.noreply.github.com>
2024-03-29 00:57:46 -0300

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+touch download-video
+
+otexto='#!/bin/bash
+if [[ $1 == 'br' ]]; then
+    yt-dlp --write-subs --sub-lang "pt.*" -o "%(title)s.%(ext)s" $2
+elif [[ $1 == 'en' ]]; then
+    yt-dlp --write-subs --sub-lang "en.*" -o "%(title)s.%(ext)s" $2
+elif [[ $1 == 'normal' ]]; then
+    yt-dlp -o "%(title)s.%(ext)s" $2
+fi'
+
+echo "$otexto" > download-video
+chmod +x download-video
+echo "alias yt='bash ~/download-video'" >> ~/.zshrc
