740bbd46f39e7c01e59544a4c7c1342a80d2e602
navet <159667110+uosq@users.noreply.github.com>
2024-08-25 18:50:00 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,9 +3,9 @@ touch download-video
 
 otexto='#!/bin/zsh
 if [[ $1 == 'br' ]]; then
-    yt-dlp --write-subs --sub-lang "pt.*" -o "%(title)s.%(ext)s" $2
+    yt-dlp --embed-subs --sub-lang "pt.*" -o "%(title)s.%(ext)s" $2
 elif [[ $1 == 'en' ]]; then
-    yt-dlp --write-subs --sub-lang "en.*" -o "%(title)s.%(ext)s" $2
+    yt-dlp --embed-subs --sub-lang "en.*" -o "%(title)s.%(ext)s" $2
 elif [[ $1 == 'normal' ]]; then
     yt-dlp -o "%(title)s.%(ext)s" $2
 fi'
